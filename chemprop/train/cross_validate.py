841e630800bc357f88dd2eff3a33b2003a9ef808
Kyle Swanson <swansonk.14@gmail.com>
2020-07-29 23:36:15 -0400

    Fix to csv

--- chemprop/train/cross_validate.py
+++ chemprop/train/cross_validate.py
@@ -81,9 +81,10 @@ def cross_validate(args: TrainArgs) -> Tuple[float, float]:
             writer.writerow([task_name, mean, std] + task_scores.tolist())
 
     # Merge and save test preds if doing cross-validation
-    all_preds = pd.concat([pd.read_csv(os.path.join(save_dir, f'fold_{fold_num}', 'test_preds.csv'))
-                           for fold_num in range(args.num_folds)])
-    all_preds.to_csv(os.path.join(save_dir, 'test_preds.csv'), index=False)
+    if args.split_type == 'cv':
+        all_preds = pd.concat([pd.read_csv(os.path.join(save_dir, f'fold_{fold_num}', 'test_preds.csv'))
+                               for fold_num in range(args.num_folds)])
+        all_preds.to_csv(os.path.join(save_dir, 'test_preds.csv'), index=False)
 
     return mean_score, std_score
 
