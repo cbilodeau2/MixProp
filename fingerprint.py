0578e548276f3711fefd764470844ce80c2aab37
Chas <charlesjmcgill@gmail.com>
2020-12-20 23:38:50 -0500

    Copied Fingerprint Code

---
+++ fingerprint.py
@@ -0,0 +1,6 @@
+"""Loads a trained model checkpoint and makes predictions on a dataset."""
+
+from chemprop.train import chemprop_fingerprint
+
+if __name__ == '__main__':
+    chemprop_fingerprint()
